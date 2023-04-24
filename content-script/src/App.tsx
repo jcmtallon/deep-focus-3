21143da8c3d85e260fbac9ffaaf7f814f8b7cf55
jcmtallon <juanca_mt@hotmail.com>
2023-04-24 22:36:32 +0200

    feat: boilerplate code first cleanup

--- content-script/src/App.tsx
+++ content-script/src/App.tsx
@@ -8,9 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Logo className="App-logo" id="App-logo" title="React logo" />
-        <p>Hello, World!</p>
-        <p>I'm a Chrome Extension Content Script!</p>
+        <p>Mission Control Dashboard</p>
       </header>
     </div>
   );
--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Logo className="App-logo" id="App-logo" title="React logo" />
-        <p>Hello, World!</p>
-        <p>I'm a Chrome Extension Popup!</p>
-      </header>
+      <h1>Some title</h1>
+        <input type="text" placeholder="site" />
+        <button>Add input</button>
+        <button>Reset list</button>
+        <ul></ul>
     </div>
   );
 }
